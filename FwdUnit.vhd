130910644954c3f047c28bad96dd5a4a482794c0
陈文潇 <chenwenxiao@live.cn>
2015-11-23 12:46:21 +0800

    Init

---
+++ FwdUnit.vhd
@@ -0,0 +1,49 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    23:31:07 11/22/2015 
+-- Design Name: 
+-- Module Name:    FwdUnit - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity FwdUnit is
+    Port ( FwdUnit_Rs : in  STD_LOGIC_VECTOR (2 downto 0);
+           FwdUnit_Rt : in  STD_LOGIC_VECTOR (2 downto 0);
+           FwdUnit_RegDistSel : in  STD_LOGIC;
+           FwdUnit_ExToMem_RegWr : in  STD_LOGIC;
+           FwdUnit_ExToMem_RegNum : in  STD_LOGIC_VECTOR (2 downto 0);
+           FwdUnit_MemToWr_RegWr : in  STD_LOGIC;
+           FwdUnit_MemToWr_RegNum : in  STD_LOGIC_VECTOR (2 downto 0);
+           FwdUnit_FwdSel : out  STD_LOGIC_VECTOR (1 downto 0));
+end FwdUnit;
+
+architecture Behavioral of FwdUnit is
+
+begin
+
+
+end Behavioral;
+
---
+++ MemToWr.vhd
@@ -0,0 +1,55 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    22:41:08 11/22/2015 
+-- Design Name: 
+-- Module Name:    MemToWr - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity MemToWr is
+    Port ( MemToWr_En_IN : in  STD_LOGIC;
+           MemToWr_RegMemSel_IN : in  STD_LOGIC;
+           MemToWr_RegWr_IN : in  STD_LOGIC;
+           MemToWr_MemVal_IN : in  STD_LOGIC_VECTOR(15 downto 0);
+           MemToWr_RegVal_IN : in  STD_LOGIC_VECTOR(15 downto 0);
+           MemToWr_RegOverflow_IN : in  STD_LOGIC;
+           MemToWr_RegNum_IN : in  STD_LOGIC_VECTOR(2 downto 0);
+           MemToWr_RegMemSel_OUT : out  STD_LOGIC;
+           MemToWr_RegWr_OUT : out  STD_LOGIC;
+           MemToWr_MemVal_OUT : out  STD_LOGIC_VECTOR(15 downto 0);
+           MemToWr_RegVal_OUT : out  STD_LOGIC_VECTOR(15 downto 0);
+           MemToWr_RegOverflow_OUT : out  STD_LOGIC;
+           MemToWr_RegNum_OUT : out  STD_LOGIC_VECTOR(2 downto 0)
+			  );
+end MemToWr;
+
+architecture Behavioral of MemToWr is
+
+begin
+
+
+end Behavioral;
+
