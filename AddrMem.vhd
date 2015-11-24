94a8b93caa7a6e9d5fb99df47c8ba309bba83f31
He Xuan <609498788@qq.com>
2015-11-24 20:37:21 +0800

    AddrMem and DataMem
    AddrMem and DataMem are almost completed.
    But I'm not sure.

--- AddrMem.vhd
+++ AddrMem.vhd
@@ -30,15 +30,100 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity AddrMem is
-    Port ( AddrMem_RESET : in  STD_LOGIC;
+    Port ( AddrMem_CLK_low : in STD_LOGIC;
+			  AddrMem_CLK_high : in STD_LOGIC;
+		     AddrMem_Reset : in  STD_LOGIC;
            AddrMem_PC : in  STD_LOGIC_VECTOR (15 downto 0);
-           AddrMem_Ins : out  STD_LOGIC_VECTOR (15 downto 0));
+			  AddrMem_RE : in STD_LOGIC;
+			  AddrMem_WE : in STD_LOGIC;
+			  AddrMem_EN : in STD_LOGIC;
+			  AddrMem_Ins_IN : in STD_LOGIC_VECTOR (15 downto 0);
+           AddrMem_Ins_OUT : out  STD_LOGIC_VECTOR (15 downto 0);
+			  
+			  AddrMem_Ram1_Addr : out STD_LOGIC_VECTOR (15 downto 0);
+			  AddrMem_Ram1_Data : inout STD_LOGIC_VECTOR (15 downto 0);
+			  AddrMem_Ram1_OE : out STD_LOGIC;
+			  AddrMem_Ram1_WE : out STD_LOGIC;
+			  AddrMem_Ram1_EN : out STD_LOGIC );
 end AddrMem;
 
 architecture Behavioral of AddrMem is
+	
+	type status is (readPre,readIns,readWait,writePre,writeIns,writeWait,Asleep);
+	signal current_status : status := Asleep;
+	signal next_status : status := Asleep;
 
 begin
 
+process( AddrMem_CLK_high,AddrMem_Reset,AddrMem_EN)
+begin
+	if AddrMem_Reset = '0' or AddrMem_EN = '1' then
+		current_status <= Asleep;
+	elsif rising_edge( AddrMem_CLK_high) and AddrMem_EN = '0' then
+		current_status <= next_status;
+	end if;
+end process;
+
+process(current_status,AddrMem_EN)
+begin	
+	case current_status is
+		when Asleep =>
+			if AddrMem_RE = '1' and AddrMem_WE = '0' and AddrMem_EN = '0' then
+				next_status <= readPre;
+			elsif AddrMem_RE = '0' and AddrMem_WE = '1' and AddrMem_EN = '0' then
+				next_status <= writePre;
+				else
+					next_status <= Asleep;
+			end if;
+		when readPre =>
+			next_status <= readIns;
+		when readIns =>
+			next_status <= readWait;
+		when readWait =>
+			next_status <= Asleep;
+		when writePre =>
+			next_status <= writeIns;
+		when writeIns =>
+			next_status <= writeWait;
+		when writeWait =>
+			next_status <= Asleep;
+		when others =>
+			next_status <= Asleep;
+	end case;
+end process;
+
+process(AddrMem_CLK_high)
+begin
+	case next_status is
+		when readPre =>
+			AddrMem_Ram1_Addr <= AddrMem_PC;
+			AddrMem_Ram1_Data <= "ZZZZZZZZZZZZZZZZ";
+			AddrMem_Ram1_EN <= '0';
+			AddrMem_Ram1_OE <= '0';
+			AddrMem_Ram1_WE <= '1';
+		when readIns =>
+			AddrMem_Ins_OUT <= AddrMem_Ram1_Data;
+		when readWait =>
+			AddrMem_Ram1_EN <= '1';
+			AddrMem_Ram1_OE <= '1';
+			AddrMem_Ram1_WE <= '1';
+		when writePre =>
+			AddrMem_Ram1_Addr <= AddrMem_PC;
+			AddrMem_Ram1_Data <= AddrMem_Ins_IN;
+			AddrMem_Ram1_EN <= '0';
+			AddrMem_Ram1_OE <= '1';
+			AddrMem_Ram1_WE <= '1';
+		when writeIns =>
+			AddrMem_Ram1_EN <= '0';
+			AddrMem_Ram1_OE <= '1';
+			AddrMem_Ram1_WE <= '0';
+		when writeWait =>
+			AddrMem_Ram1_EN <= '1';
+			AddrMem_Ram1_OE <= '1';
+			AddrMem_Ram1_WE <= '1';
+		when Asleep =>
+	end case;
+end process;
 
 end Behavioral;
 
