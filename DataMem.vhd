d26e1c3ad1e11f7c79520d31814aa54b51d84854
何轩 <609498788@qq.com>
2015-11-26 21:01:05 +0800

    DataMem Simulation Finished

--- DataMem.vhd
+++ DataMem.vhd
@@ -38,7 +38,7 @@ entity DataMem is
            DataMem_Val_IN : in  STD_LOGIC_VECTOR (15 downto 0);
            DataMem_Val_OUT : out  STD_LOGIC_VECTOR (15 downto 0);
            
-           DataMem_AddrMem_IfToId_EN : out STD_LOGIC;                                  --interface to control EN in AddrMem
+           DataMem_Stop_EN : out STD_LOGIC;                                  --interface to control EN in AddrMem
            
            DataMem_Ram1_Addr : out STD_LOGIC_VECTOR (15 downto 0);              --interface for Ram1
            DataMem_Ram1_Data : inout STD_LOGIC_VECTOR (15 downto 0);
@@ -74,51 +74,43 @@ begin
                 DataMem_Ram2_OE <= '0';
                 DataMem_Ram2_WE <= '1';
             end if;
-        elsif DataMem_RE = '0' and DataMem_WE = '1' and DataMem_EN = '1' then
-            if DataMem_Addr <= "1011111011111111" then
-                DataMem_Ram1_Addr <= DataMem_Addr;
-                DataMem_Ram1_Data <= DataMem_Val_IN;
-                DataMem_Ram1_EN <= '0';
-                DataMem_Ram1_OE <= '1';
-                DataMem_Ram1_WE <= '1';
-            else
-                DataMem_Ram2_Addr <= DataMem_Addr;
-                DataMem_Ram2_Data <= DataMem_Val_IN;
-                DataMem_Ram2_EN <= '0';
-                DataMem_Ram2_OE <= '1';
-                DataMem_Ram2_WE <= '1';
+        else
+            if DataMem_RE = '0' and DataMem_WE = '1' and DataMem_EN = '1' then
+                if DataMem_Addr <= "1011111011111111" then
+                    DataMem_Ram1_Addr <= DataMem_Addr;
+                    DataMem_Ram1_Data <= DataMem_Val_IN;
+                    DataMem_Ram1_EN <= '0';
+                    DataMem_Ram1_OE <= '1';
+                    DataMem_Ram1_WE <= '1';
+                else
+                    DataMem_Ram2_Addr <= DataMem_Addr;
+                    DataMem_Ram2_Data <= DataMem_Val_IN;
+                    DataMem_Ram2_EN <= '0';
+                    DataMem_Ram2_OE <= '1';
+                    DataMem_Ram2_WE <= '1';
+                end if;
             end if;
         end if;
-    elsif DataMem_CLK = '0' then
-        if DataMem_RE = '1' and DataMem_WE = '0' and DataMem_EN = '1' then
-            if DataMem_Addr <= "1011111011111111" then                      -- Select Ram1
-                DataMem_Ram1_Addr <= DataMem_Addr;
-                DataMem_Ram1_Data <= "ZZZZZZZZZZZZZZZZ";
-                DataMem_Ram1_EN <= '0';
-                DataMem_Ram1_OE <= '0';
-                DataMem_Ram1_WE <= '1';
-            else                                                            -- Select Ram2
-                DataMem_Ram2_Addr <= DataMem_Addr;
-                DataMem_Ram2_Data <= "ZZZZZZZZZZZZZZZZ";
-                DataMem_Ram2_EN <= '0';
-                DataMem_Ram2_OE <= '0';
-                DataMem_Ram2_WE <= '1';
-            end if;
-        elsif DataMem_RE = '0' and DataMem_WE = '1' and DataMem_EN = '1' then
-            if DataMem_Addr <= "1011111011111111" then
-                DataMem_Ram1_EN <= '0';
-                DataMem_Ram1_OE <= '1';
-                DataMem_Ram1_WE <= '0';
+    else
+        if DataMem_CLK = '0' then
+            if DataMem_RE = '1' and DataMem_WE = '0' and DataMem_EN = '1' then
+                if DataMem_Addr <= "1011111011111111" then                      -- Select Ram1
+                    DataMem_Val_OUT <= DataMem_Ram1_Data;
+                else                                                            -- Select Ram2
+                    DataMem_Val_OUT <= DataMem_Ram2_Data;
+                end if;
             else
-                DataMem_Ram2_EN <= '0';
-                DataMem_Ram2_OE <= '1';
-                DataMem_Ram2_WE <= '0';
-            end if;
-        else
-            if DataMem_Addr <= "1011111011111111" then
-                DataMem_Val_OUT <= DataMem_Ram1_Data;
-            else
-                DataMem_Val_OUT <= DataMem_Ram2_Data;
+                if DataMem_RE = '0' and DataMem_WE = '1' and DataMem_EN = '1' then
+                    if DataMem_Addr <= "1011111011111111" then
+                        DataMem_Ram1_EN <= '0';
+                        DataMem_Ram1_OE <= '1';
+                        DataMem_Ram1_WE <= '0';
+                    else
+                        DataMem_Ram2_EN <= '0';
+                        DataMem_Ram2_OE <= '1';
+                        DataMem_Ram2_WE <= '0';
+                    end if;
+                end if;
             end if;
         end if;
     end if;
@@ -127,9 +119,9 @@ end process;
 process(DataMem_RE, DataMem_WE, DataMem_EN)
 begin
     if DataMem_EN = '1' and DataMem_RE = '0' and DataMem_WE  = '1' then
-        DataMem_AddrMem_IfToId_EN <= '0';
+        DataMem_Stop_EN <= '0';
     else
-        DataMem_AddrMem_IfToId_EN <= '1';
+        DataMem_Stop_EN <= '1';
     end if;
 end process;
 
